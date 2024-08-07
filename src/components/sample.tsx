14de229962bbd0d4fdf672cfa5ccab20b37e17c3
Zainab Oyedeji <zainab@uniswitchng.com>
2024-08-07 14:34:24 +0100

    recent chnages

--- src/components/sample.tsx
+++ src/components/sample.tsx
@@ -4,61 +4,35 @@ import { useScroll } from "framer-motion";
 import { useEffect, useRef } from "react";
 import Lenis from "@studio-freight/lenis";
 import Card from "@/components/Card";
-import HandIMG from "../../public/images/beautyhub-banner.svg";
-import PercentImg from "../../public/images/beautyhub-logo.svg";
-import PhoneSVG from "../../public/images/lendsqr-banner.svg";
-import billsPayment from "../../public/images/lendsqrlogo.svg";
-import billsPaymentImg from "../../public/images/lendsqrlogo.svg";
-import savingsImg from "../../public/images/lendsqrlogo.svg";
-import buyNow from "../../public/images/lendsqrlogo.svg";
-import buyNowImg from "../../public/images/lendsqrlogo.svg";
+import BeautyHubLogo from "../../public/images/beautyhub-logo.svg";
+import BeautyHubBanner from "../../public/images/beautyhub-banner.svg";
+import LendsqrBanner from "../../public/images/lendsqr-banner.svg";
+import LendsqrLogo from "../../public/images/lendsqrlogo.svg";
 
 export const projects = [
   {
-    smallTitle: "Loan",
-    title: "Loan Marketplace",
-    url: "images/phone.svg",
+    iconImage: BeautyHubLogo.src,
+    smallTitle: "WEB DESIGN",
     description:
-      "Discover a diverse range of loan options from various lenders, all in one convenient marketplace. Whether you're seeking personal loans, business funding, or specialized financing, find the perfect solution tailored to your needs.",
-    image: PhoneSVG.src,
-    iconImage: PercentImg.src,
-    color: "#ECF4FD",
+      "Worlds first scheduling app.",
+    image: BeautyHubBanner.src,
+    color: "#FCEBEF",
   },
   {
-    smallTitle: "P2P Lending",
-    title: "Loan Out Your Excess Cash",
+    iconImage: LendsqrLogo.src,
+    smallTitle: "WEB DESIGN",
     description:
-      "Our P2P lending empowers you to lend to friends, neighbours, or colleagues on your terms. With peer-to-peer lending, foster community support while earning returns on your investments, creating a win-win scenario for all parties involved.",
-    image: HandIMG.src,
-    iconImage: false,
-    color: "#FFF6F6",
+      "Worlds first scheduling app.",
+    image: LendsqrBanner.src,
+    color: "#F4EDFF",
   },
   {
-    smallTitle: "Buy Now, Pay Later",
-    title: "Own It Now & Pay Later",
+    iconImage: BeautyHubLogo.src,
+    smallTitle: "WEB DESIGN",
     description:
-      "Embrace the freedom of purchasing gadgets, electronics, and more in convenient installments with our Buy Now Pay Later feature. Shop with ease and flexibility, managing your payments effortlessly at your fingertips.",
-    image: buyNowImg.src,
-    iconImage: buyNow.src,
-    color: "#ECF4FD",
-  },
-  {
-    smallTitle: "Booster Savings",
-    title: "Save Towards a Goal",
-    description:
-      "Start building your financial future with our savings platform. Whether you're saving for a rainy day, a dream vacation, or a major life milestone, our user-friendly savings tools make it easy to set aside funds and track your progress towards your goals.",
-    image: savingsImg.src,
-    iconImage: false,
-    color: "#EFFBF0",
-  },
-  {
-    smallTitle: "Bills Payments",
-    title: "Airtime & Bill payment",
-    description:
-      "Simplify your everyday transactions with our airtime and bills payment services. Recharge your mobile phone credits, settle utility bills, and manage your expenses seamlessly, all from the comfort of your home or on the go.",
-    image: billsPayment.src,
-    iconImage: billsPaymentImg.src,
-    color: "#ECF4FD",
+      "Worlds first scheduling app.",
+    image: BeautyHubBanner.src,
+    color: "#FCEBEF",
   },
 ];
 const Offerings = () => {
