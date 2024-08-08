0384b335d1fe022406a08b5a63522d2658f30525
Zainab Oyedeji <zainab@uniswitchng.com>
2024-08-08 11:48:23 +0100

    recent changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <main className="mt-10 bg-gradient-to-r from-[#FAF8FC] to-[#F4EFFF] p-10 rounded-2xl">
-      <h1 className="font-bold text-6xl">Send me a <br/>message</h1>
-      <div className="mt-10 flex justify-between">
-        <div>
-          <Link href="" className="underline mr-5">LinkedIn</Link>
+    <main className="mt-10 bg-gradient-to-r from-[#FAF8FC] to-[#F4EFFF] p-6 sm:p-8 md:p-10 rounded-xl sm:rounded-2xl">
+      <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl">
+        Send me a <br/>message
+      </h1>
+      <div className="mt-8 sm:mt-10 flex flex-col sm:flex-row justify-between gap-4 sm:gap-0">
+        <div className="flex flex-col sm:flex-row gap-4">
+          <Link href="" className="underline">LinkedIn</Link>
           <Link href="" className="underline">Twitter</Link>
         </div>
         <div>
--- src/components/letsworktogether.tsx
+++ src/components/letsworktogether.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+
+interface RepeatedTextProps {
+  text: string;
+  repeatCount: number;
+  imageSrc: string;
+}
 
 const LetsWorkTogether = () => {
-  const text = "Let's Work Together"
-  const repeatedText = text.repeat(10); 
-
+  const text = "Let's Work Together";
+  const RepeatedText: React.FC<RepeatedTextProps> = ({
+    text,
+    repeatCount,
+    imageSrc,
+  }) => {
+    const repeatedTextArray = Array(repeatCount).fill(text);
+    return (
+      <div className="flex items-center">
+        {repeatedTextArray.map((item, index) => (
+          <React.Fragment key={index}>
+            <Image
+              src={imageSrc}
+              width="40"
+              height="40"
+              alt="Star Icon"
+              className="mx-2"
+            />
+            <span>{item}</span>
+          </React.Fragment>
+        ))}
+        <Image
+          src={imageSrc}
+          width="40"
+          height="40"
+          alt="Star Icon"
+          className="mx-2"
+        />
+      </div>
+    );
+  };
   return (
-    <div className="overflow-hidden whitespace-nowrap">
-      <div className="scrolling-text text-black text-6xl font-bold px-6 py-2">
-        {repeatedText} 
+    <div className="overflow-hidden whitespace-nowrap ">
+      <div className="scrolling-text text-black text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold px-4 sm:px-6 py-2">
+        <RepeatedText
+          text="Let's Work Together"
+          repeatCount={10}
+          imageSrc="/images/star-green.svg"
+        />
       </div>
     </div>
   );
